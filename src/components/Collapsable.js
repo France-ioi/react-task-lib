cbf3c22c057ced65192b123f9ae51f77ca49ec28
Arkandias <arkandias1@hotmail.fr>
2021-02-05 18:32:41 +0100

    Add closable tools

--- src/components/Collapsable.js
+++ src/components/Collapsable.js
@@ -1,13 +1,15 @@
 import React, {useState} from 'react';
 import {useStore} from 'react-redux'
-import {Collapse} from 'react-bootstrap';
+import {Button, Collapse, Modal} from 'react-bootstrap';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 export default React.memo(function Collapsable (props) {
+  const {title, children, tutorial, closable, onClose, closeConfirmText} = props;
+
   const [open, setOpen] = useState(true);
   const [tutorialOpen, setTutorialOpen] = useState(false);
-  const {title, children} = props;
   const [previousVersion, setPreviousVersion] = useState(null);
+  const [confirmCloseModal, setConfirmCloseModal] = useState(false);
 
   const store = useStore();
   store.subscribe(() => {
@@ -28,29 +30,58 @@ export default React.memo(function Collapsable (props) {
     setTutorialOpen(!tutorialOpen);
   };
 
+  const onClickClose = (event) => {
+    event.stopPropagation();
+    setConfirmCloseModal(true);
+  };
+
+  const doClose = () => {
+    setConfirmCloseModal(false);
+    onClose();
+  };
+
   const newTitle = React.cloneElement(title, {
     onClick: () => setOpen(!open),
     children: [
       title.props.children,
       " ",
-      ...(props.tutorial ? [
+      ...(tutorial ? [
         <a className="tutorial-link" key="icon-tutorial" onClick={(e) => toggleTutorial(e)}>
           {tutorialOpen ? 'Cacher' : 'Lire'} les explications
           <FontAwesomeIcon className="icon-tutorial" icon={tutorialOpen ? 'chevron-up' : 'chevron-down'} size="xs"/>
         </a>
         ] : []),
+      closable ? <FontAwesomeIcon key="icon-close" className="icon-close" icon="times" onClick={onClickClose}/> : null,
       <FontAwesomeIcon key="icon-collapse" className="icon-collapse" icon={open ? 'chevron-up' : 'chevron-down'} />,
     ]
   });
 
   return (
     <React.Fragment>
+      <Modal
+        show={confirmCloseModal}
+        onHide={() => setConfirmCloseModal(false)}
+        size="lg"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>
+            Confirmation
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>{closeConfirmText}</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setConfirmCloseModal(false)}>Annuler</Button>
+          <Button variant="primary" onClick={() => doClose()}>Confirmer</Button>
+        </Modal.Footer>
+      </Modal>
       {newTitle}
       <Collapse in={open}>
         <div>
           <Collapse in={tutorialOpen}>
             <div className="carousel-container">
-              {props.tutorial}
+              {tutorial}
             </div>
           </Collapse>
           {children}
