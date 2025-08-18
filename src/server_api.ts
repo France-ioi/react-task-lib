349128abcd18ad7b75168336a4055dbe88f75108
Sébastien Tainon <arkandias1@hotmail.fr>
2025-08-18 18:18:12 +0200

    Add long polling for distant server with slow executions

--- src/server_api.ts
+++ src/server_api.ts
@@ -1,22 +1,52 @@
+async function makeRequestLongPolling(url: URL, totalBody: any) {
+  const response = await fetch(url.toString(), {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    },
+    body: JSON.stringify(totalBody),
+  });
+  if (response.status !== 200) {
+    throw new Error(`Invalid response status: ${response.status}`);
+  }
+
+  return await response.json();
+}
+
 export default function makeServerApi (config) {
     return function (service, action, body) {
-        return new Promise(function (resolve, reject) {
+        return new Promise(async function (resolve, reject) {
             const url = new URL(service, config.baseUrl);
             const devel = config.devel ? {task: config.devel} : {};
-            return fetch(url.toString(), {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json',
-                },
-                body: JSON.stringify({...body, ...devel, action})
-            }).then(function (response) {
-                if (response.status !== 200) return reject(response);
-                response.json().catch(reject).then(function (result) {
-                    if (!result.success) return reject(result.error);
-                    resolve(result.data);
-                });
-            }).catch(reject);
+
+            const maxTries = 30;
+            let longPollingId = null;
+            for (let i = 0; i < maxTries; i++) {
+              try {
+                let totalBody = {
+                  ...body,
+                  ...devel,
+                  action,
+                  ...(longPollingId ? {long_polling_id: longPollingId} : {}),
+                };
+
+                const result = await makeRequestLongPolling(url, totalBody);
+                if (result.data?.longPolling) {
+                  longPollingId = result.data?.longPollingFollowUpId;
+                } else if (!result.success) {
+                  reject(result.error);
+                  return;
+                } else {
+                  resolve(result.data);
+                  return;
+                }
+                // @ts-ignore
+              } catch (e: Error) {
+                reject(e.message);
+                return;
+              }
+            }
         });
     };
 }
