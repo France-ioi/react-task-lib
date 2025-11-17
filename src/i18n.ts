155fc59af2b592a0ecb394ebdfaa1290c72942ee
Sébastien Tainon <arkandias1@hotmail.fr>
2025-11-17 16:05:32 +0100

    Add german and spanish translations

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,11 +2,15 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import frJSON from './lang/fr.json';
 import enJSON from './lang/en.json';
+import deJSON from './lang/de.json';
+import esJSON from './lang/es.json';
 
 i18n.use(initReactI18next).init({
   resources: {
     fr: { ...frJSON },
     en: { ...enJSON },
+    de: { ...deJSON },
+    es: { ...esJSON },
   },
   lng: "fr",
 });
