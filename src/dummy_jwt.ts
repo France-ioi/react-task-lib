d1c747085b4cdbc314e6ccb91fc7e2c285cc6180
Hugo Peyraud-Magnin <hugo.pm@pm.me>
2026-02-20 11:42:56 +0900

    Fix dummy jwt decode64 and don't use window.jwt

--- src/dummy_jwt.ts
+++ src/dummy_jwt.ts
@@ -1,31 +1,39 @@
 // Code extracted from miniPlatform.js
+function sign(data: any, ..._args: any[]) {
+  var header = { alg: "HS256", typ: "JWT" };
+  // Dummy signature because it would require
+  // installing a crypto library to calculate the real one
+  var signature = 'bcM4TMh3PG77_0P7DwqeUAR07XKIvgNce58uJEEP_6A';
+  var claim = Object.assign(data, { "sub": 1234567890 });
+
+  return encode64(JSON.stringify(header)) + '.' +
+  encode64(JSON.stringify(claim)) + '.' +
+  (signature || '');
+}
+function decode(token: string) {
+  var parts = token.split('.');
+  return JSON.parse(decode64(parts[1]));
+}
+function encode64(value: string) {
+  // encode in base64
+  var encoded = btoa(unescape(encodeURIComponent(value)));
+  // base64 -> base64url
+  return encoded
+    .replace(/\//g, '_')
+    .replace(/\+/g, '-')
+    .replace(/=+$/g, '');
+}
+function decode64(value: string) {
+  // base64url -> base64 (padding is optional for atob)
+  var v = value.replace(/-/g, '+').replace(/_/g, '/');
+  // decode base64
+  return decodeURIComponent(escape(atob(v)));
+}
+
 export const jwt = {
-    isDummy: true,
-    sign: function (data: any, ..._args: any[]) {
-        var header = { alg: "HS256", typ: "JWT" };
-        // Dummy signature because it would require
-        // installing a crypto library to calculate the real one
-        var signature = 'bcM4TMh3PG77_0P7DwqeUAR07XKIvgNce58uJEEP_6A';
-        var claim = Object.assign(data, { "sub": 1234567890 });
-
-        return window.jwt.encode64(JSON.stringify(header)) + '.' +
-            window.jwt.encode64(JSON.stringify(claim)) + '.' +
-            (signature || '');
-    },
-    decode: function (token: string) {
-        var parts = token.split('.');
-
-        return JSON.parse(window.jwt.decode64(parts[1]));
-    },
-    encode64: function (value: string) {
-        var encoded = btoa(unescape(encodeURIComponent(value)));
-
-        return encoded
-            .replace(/\//g, '_')
-            .replace(/\+/g, '-')
-            .replace(/=+$/g, '');
-    },
-    decode64: function (value: string) {
-        return decodeURIComponent(escape(atob(value)));
-    }
+  isDummy: true,
+  sign,
+  decode,
+  encode64,
+  decode64
 }
